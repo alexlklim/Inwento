597d0533633e4d8ad32c7e0198a13051b1d20575
Alex Klim <91628959+alexlklim@users.noreply.github.com>
2024-03-11 08:09:32 +0100

    ALEX: add controller advice and exception for NOT_FOUND

---
+++ src/main/java/com/alex/asset/utils/expceptions/ExceptionBody.java
@@ -0,0 +1,20 @@
+package com.alex.asset.utils.expceptions;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Map;
+
+@Getter
+@Setter
+public class ExceptionBody {
+    private Integer code;
+    private String message;
+
+    public ExceptionBody(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
