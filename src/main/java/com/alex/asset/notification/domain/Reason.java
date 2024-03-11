ffed50d6c00cc8b408d81bb824097ad9e2c740f7
Alex Klim <91628959+alexlklim@users.noreply.github.com>
2024-03-11 10:31:47 +0100

    ALEX: add logic for notification section

--- src/main/java/com/alex/asset/notification/domain/Reason.java
+++ src/main/java/com/alex/asset/notification/domain/Reason.java
@@ -2,7 +2,16 @@ package com.alex.asset.notification.domain;
 
 public enum Reason {
 
-    INVENT, CONTROL, NEED_TO_APPROVE, HIDE_OPERATION
+    INVENT, CONTROL, NEED_TO_APPROVE, HIDE_OPERATION;
 
+
+    public static Reason fromString(String value) {
+        for (Reason reason : Reason.values()) {
+            if (reason.name().equalsIgnoreCase(value)) {
+                return reason;
+            }
+        }
+        throw new IllegalArgumentException("No constant with value " + value + " found in enum Reason");
+    }
 
 }
