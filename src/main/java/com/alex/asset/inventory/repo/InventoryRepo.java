bcd7d326bb75577de985e5e9003b34f4cfae1e1f
Alex Klim <91628959+alexlklim@users.noreply.github.com>
2024-04-19 10:22:19 +0200

    ALEX: -  removed unused imports

--- src/main/java/com/alex/asset/inventory/repo/InventoryRepo.java
+++ src/main/java/com/alex/asset/inventory/repo/InventoryRepo.java
@@ -3,7 +3,6 @@ package com.alex.asset.inventory.repo;
 import com.alex.asset.inventory.domain.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.Optional;
