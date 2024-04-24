98426db45275597a3ce96329b16ccf796f77a110
Alex Klim <91628959+alexlklim@users.noreply.github.com>
2024-04-24 12:10:05 +0200

    ALEX: update versions of tools

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,20 +36,20 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.activity:activity:1.8.0")
+    implementation("androidx.activity:activity:1.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     // for json data binding
-    implementation ("com.google.code.gson:gson:2.8.8")
+    implementation ("com.google.code.gson:gson:2.10.1")
 
     // for scaning
     compileOnly("com.symbol:emdk:+")
-    implementation("com.google.android.gms:play-services-location:21.0.1")
+    implementation("com.google.android.gms:play-services-location:21.2.0")
 
 
-    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.11.0"))
+    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.12.0"))
     implementation("com.squareup.okhttp3:okhttp")
     implementation("com.squareup.okhttp3:logging-interceptor")
 
@@ -60,7 +60,6 @@ dependencies {
     annotationProcessor("androidx.room:room-compiler:2.6.1")
 
     // Lombok dependency
-    implementation("org.projectlombok:lombok:1.18.20")
-    annotationProcessor("org.projectlombok:lombok:1.18.20")
-    
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
+
 }
