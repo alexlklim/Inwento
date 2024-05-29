0032f95b43a3c63453d85685115801b4f52c46ae
Alex Klim <91628959+alexlklim@users.noreply.github.com>
2024-05-29 15:51:47 +0200

    ALEX: - replace switch case handling with Supplier

--- src/main/java/com/alex/asset/company/service/CompanyMapper.java
+++ src/main/java/com/alex/asset/company/service/CompanyMapper.java
@@ -3,112 +3,47 @@ package com.alex.asset.company.service;
 import com.alex.asset.company.domain.Company;
 import com.alex.asset.utils.dictionaries.UtilCompany;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Supplier;
 
 public class CompanyMapper {
-
-
     public static Map<String, Object> toDTOWithCustomFields(
-            Map<String, Object> dtoMap, Company company, List<String> fields) {
-        for (String field : fields) {
-            switch (field) {
-                case UtilCompany.COMPANY:
-                    dtoMap.put(UtilCompany.COMPANY, company.getCompany() != null ? company.getCompany() : "");
-                    break;
-                case UtilCompany.CITY:
-                    dtoMap.put(UtilCompany.CITY, company.getCity() != null ? company.getCity() : "");
-                    break;
-                case UtilCompany.STREET:
-                    dtoMap.put(UtilCompany.STREET, company.getStreet() != null ? company.getStreet() : "");
-                    break;
-                case UtilCompany.ZIP_CODE:
-                    dtoMap.put(UtilCompany.ZIP_CODE, company.getZipCode() != null ? company.getZipCode() : "");
-                    break;
-                case UtilCompany.NIP:
-                    dtoMap.put(UtilCompany.NIP, company.getNip() != null ? company.getNip() : "");
-                    break;
-                case UtilCompany.REGON:
-                    dtoMap.put(UtilCompany.REGON, company.getRegon() != null ? company.getRegon() : "");
-                    break;
-                case UtilCompany.PHONE:
-                    dtoMap.put(UtilCompany.PHONE, company.getPhone() != null ? company.getPhone() : "");
-                    break;
-                case UtilCompany.EMAIL:
-                    dtoMap.put(UtilCompany.EMAIL, company.getEmail() != null ? company.getEmail() : "");
-                    break;
-                case UtilCompany.LABEL_WIDTH:
-                    dtoMap.put(UtilCompany.LABEL_WIDTH, company.getLabelWidth());
-                    break;
-                case UtilCompany.LABEL_HEIGHT:
-                    dtoMap.put(UtilCompany.LABEL_HEIGHT, company.getLabelHeight());
-                    break;
-                case UtilCompany.LABEL_TYPE:
-                    dtoMap.put(UtilCompany.LABEL_TYPE, company.getLabelType());
-                    break;
+            Map<String, Object> dtoMap, Company company, List<String> fields
+    ) {
+        Map<String, Supplier<Object>> dataFetchers = new HashMap<>();
+        dataFetchers.put(UtilCompany.COMPANY, company::getCompany);
+        dataFetchers.put(UtilCompany.CITY, company::getCity);
+        dataFetchers.put(UtilCompany.STREET, company::getStreet);
+        dataFetchers.put(UtilCompany.ZIP_CODE, company::getZipCode);
+        dataFetchers.put(UtilCompany.NIP, company::getNip);
+        dataFetchers.put(UtilCompany.REGON, company::getRegon);
+        dataFetchers.put(UtilCompany.PHONE, company::getPhone);
+        dataFetchers.put(UtilCompany.EMAIL, company::getEmail);
+        dataFetchers.put(UtilCompany.LABEL_WIDTH, company::getLabelWidth);
+        dataFetchers.put(UtilCompany.LABEL_HEIGHT, company::getLabelHeight);
+        dataFetchers.put(UtilCompany.LABEL_TYPE, company::getLabelType);
+        dataFetchers.put(UtilCompany.EMAIL_HOST, company::getHost);
+        dataFetchers.put(UtilCompany.EMAIL_PORT, company::getPort);
+        dataFetchers.put(UtilCompany.EMAIL_USERNAME, company::getUsername);
+        dataFetchers.put(UtilCompany.EMAIL_PASSWORD, company::getPassword);
+        dataFetchers.put(UtilCompany.EMAIL_PROTOCOL, company::getProtocol);
+        dataFetchers.put(UtilCompany.EMAIL_CONFIGURED, company::getIsEmailConfigured);
+        dataFetchers.put(UtilCompany.BAR_CODE_LENGTH, company::getBarCodeLength);
+        dataFetchers.put(UtilCompany.BAR_CODE_LENGTH_MAX, company::getBarCodeLengthMax);
+        dataFetchers.put(UtilCompany.BAR_CODE_LENGTH_MIN, company::getBarCodeLengthMin);
+        dataFetchers.put(UtilCompany.BAR_CODE_PREFIX, company::getBarCodePrefix);
+        dataFetchers.put(UtilCompany.BAR_CODE_SUFFIX, company::getBarCodeSuffix);
+        dataFetchers.put(UtilCompany.BAR_CODE_POSTFIX, company::getBarCodePostfix);
+        dataFetchers.put(UtilCompany.RFID_LENGTH, company::getRfidCodeLength);
+        dataFetchers.put(UtilCompany.RFID_LENGTH_MAX, company::getRfidCodeLengthMax);
+        dataFetchers.put(UtilCompany.RFID_LENGTH_MIN, company::getRfidCodeLengthMin);
+        dataFetchers.put(UtilCompany.RFID_PREFIX, company::getRfidCodePrefix);
+        dataFetchers.put(UtilCompany.RFID_SUFFIX, company::getRfidCodeSuffix);
+        dataFetchers.put(UtilCompany.RFID_POSTFIX, company::getRfidCodePostfix);
 
-                case UtilCompany.EMAIL_HOST:
-                    dtoMap.put(UtilCompany.EMAIL_HOST, company.getHost() != null ? company.getHost() : "");
-                    break;
-                case UtilCompany.EMAIL_PORT:
-                    dtoMap.put(UtilCompany.EMAIL_PORT, company.getPort() != null ? company.getPort() : "");
-                    break;
-                case UtilCompany.EMAIL_USERNAME:
-                    dtoMap.put(UtilCompany.EMAIL_USERNAME, company.getUsername() != null ? company.getUsername() : "");
-                    break;
-                case UtilCompany.EMAIL_PASSWORD:
-                    dtoMap.put(UtilCompany.EMAIL_PASSWORD, company.getPassword() != null ? company.getPassword() : "");
-                    break;
-                case UtilCompany.EMAIL_PROTOCOL:
-                    dtoMap.put(UtilCompany.EMAIL_PROTOCOL, company.getProtocol() != null ? company.getProtocol() : "");
-                    break;
-                case UtilCompany.EMAIL_CONFIGURED:
-                    dtoMap.put(UtilCompany.EMAIL_CONFIGURED, company.getIsEmailConfigured());
-                    break;
-
-
-                case UtilCompany.BAR_CODE_LENGTH:
-                    dtoMap.put(UtilCompany.BAR_CODE_LENGTH, company.getBarCodeLength() != null ? company.getBarCodeLength() : "");
-                    break;
-                case UtilCompany.BAR_CODE_LENGTH_MAX:
-                    dtoMap.put(UtilCompany.BAR_CODE_LENGTH_MAX, company.getBarCodeLengthMax() != null ? company.getBarCodeLengthMax() : "");
-                    break;
-                case UtilCompany.BAR_CODE_LENGTH_MIN:
-                    dtoMap.put(UtilCompany.BAR_CODE_LENGTH_MIN, company.getBarCodeLengthMin() != null ? company.getBarCodeLengthMin() : "");
-                    break;
-                case UtilCompany.BAR_CODE_PREFIX:
-                    dtoMap.put(UtilCompany.BAR_CODE_PREFIX, company.getBarCodePrefix() != null ? company.getBarCodePrefix() : "");
-                    break;
-                case UtilCompany.BAR_CODE_SUFFIX:
-                    dtoMap.put(UtilCompany.BAR_CODE_SUFFIX, company.getBarCodeSuffix() != null ? company.getBarCodeSuffix() : "");
-                    break;
-                case UtilCompany.BAR_CODE_POSTFIX:
-                    dtoMap.put(UtilCompany.BAR_CODE_POSTFIX, company.getBarCodePostfix() != null ? company.getBarCodePostfix() : "");
-                    break;
-
-
-                case UtilCompany.RFID_LENGTH:
-                    dtoMap.put(UtilCompany.RFID_LENGTH, company.getRfidCodeLength() != null ? company.getRfidCodeLength() : "");
-                    break;
-                case UtilCompany.RFID_LENGTH_MAX:
-                    dtoMap.put(UtilCompany.RFID_LENGTH_MAX, company.getRfidCodeLengthMax() != null ? company.getRfidCodeLengthMax() : "");
-                    break;
-                case UtilCompany.RFID_LENGTH_MIN:
-                    dtoMap.put(UtilCompany.RFID_LENGTH_MIN, company.getRfidCodeLengthMin() != null ? company.getRfidCodeLengthMin() : "");
-                    break;
-                case UtilCompany.RFID_PREFIX:
-                    dtoMap.put(UtilCompany.RFID_PREFIX, company.getRfidCodePrefix() != null ? company.getRfidCodePrefix() : "");
-                    break;
-                case UtilCompany.RFID_SUFFIX:
-                    dtoMap.put(UtilCompany.RFID_SUFFIX, company.getRfidCodeSuffix() != null ? company.getRfidCodeSuffix() : "");
-                    break;
-                case UtilCompany.RFID_POSTFIX:
-                    dtoMap.put(UtilCompany.RFID_POSTFIX, company.getRfidCodePostfix() != null ? company.getRfidCodePostfix() : "");
-                    break;
-
-                default: break;
-            }
-        }
+        fields.forEach(field -> dtoMap.put(field, dataFetchers.getOrDefault(field, () -> "").get()));
         return dtoMap;
     }
 
--- src/main/java/com/alex/asset/product/mappers/ProductMapper.java
+++ src/main/java/com/alex/asset/product/mappers/ProductMapper.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Supplier;
 
 
 @Service
@@ -17,132 +18,79 @@ public class ProductMapper {
         dtoMap.put(UtilProduct.ID, product.getId());
         dtoMap.put(UtilProduct.ACTIVE, product.isActive());
         dtoMap.put(UtilProduct.TITLE, product.getTitle());
-        for (String field : fields) {
-            switch (field) {
-                case UtilProduct.DESCRIPTION:
-                    dtoMap.put(UtilProduct.TITLE, product.getDescription()!= null ? product.getDescription() : "");
-                    break;
-                case UtilProduct.PRICE:
-                    dtoMap.put(UtilProduct.PRICE, product.getPrice()!= null ? product.getPrice() : "");
-                    break;
-                case UtilProduct.BAR_CODE:
-                    dtoMap.put(UtilProduct.BAR_CODE, product.getBarCode()!= null ? product.getBarCode() : "");
-                    break;
-                case UtilProduct.RFID_CODE:
-                    dtoMap.put(UtilProduct.RFID_CODE, product.getRfidCode()!= null ? product.getRfidCode() : "");
-                    break;
-                case UtilProduct.INVENTORY_NUMBER:
-                    dtoMap.put(UtilProduct.INVENTORY_NUMBER, product.getInventoryNumber()!= null ? product.getInventoryNumber() : "");
-                    break;
-                case UtilProduct.SERIAL_NUMBER:
-                    dtoMap.put(UtilProduct.SERIAL_NUMBER, product.getSerialNumber()!= null ? product.getSerialNumber() : "");
-                    break;
-                case UtilProduct.LIABLE_ID:
-                    dtoMap.put(UtilProduct.LIABLE_ID, product.getLiable() != null ? product.getLiable().getId() : "");
-                    break;
-                case UtilProduct.LIABLE_NAME:
-                    dtoMap.put(
-                            UtilProduct.LIABLE_NAME,
-                            (product.getLiable() != null && product.getLiable().getFirstname() != null && product.getLiable().getLastname() != null) ?
-                                    product.getLiable().getFirstname() + " " + product.getLiable().getLastname() : "");
-                    break;
+        Map<String, Supplier<Object>> dataFetchers = new HashMap<>();
+        dataFetchers.put(UtilProduct.DESCRIPTION, product::getDescription);
+        dataFetchers.put(UtilProduct.PRICE, product::getPrice);
+        dataFetchers.put(UtilProduct.BAR_CODE, product::getBarCode);
+        dataFetchers.put(UtilProduct.RFID_CODE, product::getRfidCode);
+        dataFetchers.put(UtilProduct.INVENTORY_NUMBER, product::getInventoryNumber);
+        dataFetchers.put(UtilProduct.SERIAL_NUMBER, product::getSerialNumber);
+        dataFetchers.put(UtilProduct.LIABLE_ID,
+                () -> product.getLiable() != null ? product.getLiable().getId() : "");
+        dataFetchers.put(UtilProduct.LIABLE_NAME,
+                () -> (product.getLiable() != null
+                        && product.getLiable().getFirstname() != null
+                        && product.getLiable().getLastname() != null) ?
+                product.getLiable().getFirstname() + " " + product.getLiable().getLastname() : "");
+        dataFetchers.put(UtilProduct.RECEIVER, () -> product.getReceiver() != null ? product.getReceiver() : "");
+        dataFetchers.put(UtilProduct.KST_ID,
+                () -> product.getKst() != null ? product.getKst().getId() : "");
+        dataFetchers.put(UtilProduct.ASSET_STATUS_ID,
+                () -> product.getAssetStatus() != null ? product.getAssetStatus().getId() : "");
+        dataFetchers.put(UtilProduct.UNIT_ID,
+                () -> product.getUnit() != null ? product.getUnit().getId() : "");
+        dataFetchers.put(UtilProduct.BRANCH_ID,
+                () -> product.getBranch() != null ? product.getBranch().getId() : "");
+        dataFetchers.put(UtilProduct.LOCATION_ID,
+                () -> product.getLocation() != null ? product.getLocation().getId() : "");
+        dataFetchers.put(UtilProduct.MPK_ID,
+                () -> product.getMpk() != null ? product.getMpk().getId() : "");
+        dataFetchers.put(UtilProduct.TYPE_ID,
+                () -> product.getType() != null ? product.getType().getId() : "");
+        dataFetchers.put(UtilProduct.SUBTYPE_ID,
+                () -> product.getSubtype() != null ? product.getSubtype().getId() : "");
+        dataFetchers.put(UtilProduct.KST,
+                () -> product.getKst() != null ? product.getKst().getKst() : "");
+        dataFetchers.put(UtilProduct.ASSET_STATUS,
+                () -> product.getAssetStatus() != null ? product.getAssetStatus().getAssetStatus() : "");
+        dataFetchers.put(UtilProduct.UNIT,
+                () -> product.getUnit() != null ? product.getUnit().getUnit() : "");
+        dataFetchers.put(UtilProduct.BRANCH,
+                () -> product.getBranch() != null ? product.getBranch().getBranch() : "");
+        dataFetchers.put(UtilProduct.LOCATION,
+                () -> product.getLocation() != null ? product.getLocation().getLocation() : "");
+        dataFetchers.put(UtilProduct.MPK,
+                () -> product.getMpk() != null ? product.getMpk().getMpk() : "");
+        dataFetchers.put(UtilProduct.TYPE,
+                () -> product.getType() != null ? product.getType().getType() : "");
+        dataFetchers.put(UtilProduct.SUBTYPE,
+                () -> product.getSubtype() != null ? product.getSubtype().getSubtype() : "");
+        dataFetchers.put(UtilProduct.PRODUCER, product::getProducer);
+        dataFetchers.put(UtilProduct.SUPPLIER, product::getSupplier);
+        dataFetchers.put(UtilProduct.SCRAPPING, product::isScrapping);
+        dataFetchers.put(UtilProduct.SCRAPPING_DATE,
+                () -> product.getScrappingDate() != null ? product.getScrappingDate() : "");
+        dataFetchers.put(UtilProduct.SCRAPPING_REASON,
+                () -> product.getScrappingReason() != null ? product.getScrappingReason() : "");
+        dataFetchers.put(UtilProduct.DOCUMENT,
+                () -> product.getDocument() != null ? product.getDocument() : "");
+        dataFetchers.put(UtilProduct.DOCUMENT_DATE,
+                () -> product.getDocumentDate() != null ? product.getDocumentDate() : "");
+        dataFetchers.put(UtilProduct.WARRANTY_PERIOD,
+                () -> product.getWarrantyPeriod() != null ? product.getWarrantyPeriod() : "");
+        dataFetchers.put(UtilProduct.INSPECTION_DATE,
+                () -> product.getInspectionDate() != null ? product.getInspectionDate() : "");
+        dataFetchers.put(UtilProduct.LONGITUDE,
+                () -> product.getLongitude() != null ? product.getLongitude() : "");
+        dataFetchers.put(UtilProduct.LATITUDE,
+                () -> product.getLatitude() != null ? product.getLatitude() : "");
+        dataFetchers.put(UtilProduct.COMMENTS,
+                () -> product.getComments() != null ? CommentMapper.toDTOs(product.getComments()) : "");
+        dataFetchers.put(UtilProduct.HISTORY,
+                () -> product.getProductHistories() != null ?
+                        ProductHistoryMapper.toDTOs(product.getProductHistories()) : "");
 
-
-                case UtilProduct.RECEIVER:
-                    dtoMap.put(UtilProduct.RECEIVER, product.getReceiver()!= null ? product.getReceiver() : "");
-                    break;
-                case UtilProduct.KST_ID:
-                    dtoMap.put(UtilProduct.KST_ID, product.getKst() != null ? product.getKst().getId() : "");
-                    break;
-                case UtilProduct.ASSET_STATUS_ID:
-                    dtoMap.put(UtilProduct.ASSET_STATUS_ID, product.getAssetStatus() != null ? product.getAssetStatus().getId() : "");
-                    break;
-                case UtilProduct.UNIT_ID:
-                    dtoMap.put(UtilProduct.UNIT_ID, product.getUnit() != null ? product.getUnit().getId() : "");
-                    break;
-                case UtilProduct.BRANCH_ID:
-                    dtoMap.put(UtilProduct.BRANCH_ID, product.getBranch() != null ? product.getBranch().getId() : "");
-                    break;
-                case UtilProduct.LOCATION_ID:
-                    dtoMap.put(UtilProduct.LOCATION_ID, product.getLocation() != null ? product.getLocation().getId() : "");
-                    break;
-                case UtilProduct.MPK_ID:
-                    dtoMap.put(UtilProduct.MPK_ID, product.getMpk() != null ? product.getMpk().getId() : "");
-                    break;
-                case UtilProduct.TYPE_ID:
-                    dtoMap.put(UtilProduct.TYPE_ID, product.getType() != null ? product.getType().getId() : "");
-                    break;
-                case UtilProduct.SUBTYPE_ID:
-                    dtoMap.put(UtilProduct.SUBTYPE_ID, product.getSubtype() != null ? product.getSubtype().getId() : "");
-                    break;
-
-                case UtilProduct.KST:
-                    dtoMap.put(UtilProduct.KST, product.getKst() != null ? product.getKst().getKst() : "");
-                    break;
-                case UtilProduct.ASSET_STATUS:
-                    dtoMap.put(UtilProduct.ASSET_STATUS, product.getAssetStatus() != null ? product.getAssetStatus().getAssetStatus() : "");
-                    break;
-                case UtilProduct.UNIT:
-                    dtoMap.put(UtilProduct.UNIT, product.getUnit() != null ? product.getUnit().getUnit() : "");
-                    break;
-                case UtilProduct.BRANCH:
-                    dtoMap.put(UtilProduct.BRANCH, product.getBranch() != null ? product.getBranch().getBranch() : "");
-                    break;
-                case UtilProduct.LOCATION:
-                    dtoMap.put(UtilProduct.LOCATION, product.getLocation() != null ? product.getLocation().getLocation() : "");
-                    break;
-                case UtilProduct.MPK:
-                    dtoMap.put(UtilProduct.MPK, product.getMpk() != null ? product.getMpk().getMpk() : "");
-                    break;
-                case UtilProduct.TYPE:
-                    dtoMap.put(UtilProduct.TYPE, product.getType() != null ? product.getType().getType() : "");
-                    break;
-                case UtilProduct.SUBTYPE:
-                    dtoMap.put(UtilProduct.SUBTYPE, product.getSubtype() != null ? product.getSubtype().getSubtype() : "");
-                    break;
-                case UtilProduct.PRODUCER:
-                    dtoMap.put(UtilProduct.PRODUCER, product.getProducer()!= null ? product.getProducer() : "");
-                    break;
-                case UtilProduct.SUPPLIER:
-                    dtoMap.put(UtilProduct.SUPPLIER, product.getSupplier() != null ? product.getSupplier() : "");
-                    break;
-                case UtilProduct.SCRAPPING:
-                    dtoMap.put(UtilProduct.SCRAPPING, product.isScrapping());
-                    break;
-                case UtilProduct.SCRAPPING_DATE:
-                    dtoMap.put(UtilProduct.SCRAPPING_DATE, product.getScrappingDate()!= null ? product.getScrappingDate() : "");
-                    break;
-                case UtilProduct.SCRAPPING_REASON:
-                    dtoMap.put(UtilProduct.SCRAPPING_REASON, product.getScrappingReason()!= null ? product.getScrappingReason() : "");
-                    break;
-                case UtilProduct.DOCUMENT:
-                    dtoMap.put(UtilProduct.DOCUMENT, product.getDocument()!= null ? product.getDocument() : "");
-                    break;
-                case UtilProduct.DOCUMENT_DATE:
-                    dtoMap.put(UtilProduct.DOCUMENT_DATE, product.getDocumentDate()!= null ? product.getDocumentDate() : "");
-                    break;
-                case UtilProduct.WARRANTY_PERIOD:
-                    dtoMap.put(UtilProduct.WARRANTY_PERIOD, product.getWarrantyPeriod()!= null ? product.getWarrantyPeriod() : "");
-                    break;
-                case UtilProduct.INSPECTION_DATE:
-                    dtoMap.put(UtilProduct.INSPECTION_DATE, product.getInspectionDate()!= null ? product.getInspectionDate() : "");
-                    break;
-                case UtilProduct.LONGITUDE:
-                    dtoMap.put(UtilProduct.LONGITUDE, product.getLongitude()!= null ? product.getLongitude() : "");
-                    break;
-                case UtilProduct.LATITUDE:
-                    dtoMap.put(UtilProduct.LATITUDE, product.getLatitude()!= null ? product.getLatitude() : "");
-                    break;
-                case UtilProduct.COMMENTS:
-                    dtoMap.put(UtilProduct.COMMENTS, product.getComments() != null ? CommentMapper.toDTOs(product.getComments()) : "");
-                    break;
-                case UtilProduct.HISTORY:
-                    dtoMap.put(UtilProduct.HISTORY, product.getProductHistories() != null ? ProductHistoryMapper.toDTOs(product.getProductHistories()) : "");
-                    break;
-                default:
-                    break;
-            }
-        }
+        fields.forEach(field -> dtoMap.put(field, dataFetchers.getOrDefault(field, () -> "").get()));
         return dtoMap;
     }
 }
